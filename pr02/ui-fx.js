0ecc5da839f286337407a2a9cb0f0a26d559d220
Davi Inácio <aazz6850@gmail.com>
2021-04-12 00:44:02 -0300

    pr02

---
+++ pr02/ui-fx.js
@@ -0,0 +1,24 @@
+(function(root, factory) {
+	try {
+    // commonjs
+    if (typeof exports === 'object') {
+      module.exports = factory();
+    // global
+    } else {
+      root.uiFx = factory();
+    }
+  } catch(error) {
+    console.log('Isomorphic compatibility is not supported at this time for uiFx.')
+  }
+})(this, function() {
+	if (document.readyState === 'complete')
+		init();
+	else
+		window.addEventListener('DOMContentLoaded', init);
+	
+	function init(){
+		document.querySelectorAll('*[ui-fx="fadein"]').forEach((element) => {
+			console.log(element);
+		});
+	}
+});
